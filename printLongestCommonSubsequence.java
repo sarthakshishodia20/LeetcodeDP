9445262f9cdf1c2b178cd4ff56e5ab7de126ae2b
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-01-12 13:23:47 +0530

    Create printLongestCommonSubsequence.java

---
+++ printLongestCommonSubsequence.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public static String findLCS(int n, int m, String s1, String s2) {
+        // Initialize DP table
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = n, j = m;
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                sb.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    i--;
+                } else {
+                    j--;
+                }
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
