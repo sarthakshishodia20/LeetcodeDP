c0d16d132accad98c588f2683cb71ab8a3773420
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-04-02 15:11:17 +0530

    Create printingLongestCommonSubsequence.java

---
+++ printingLongestCommonSubsequence.java
@@ -0,0 +1,42 @@
+class LCS {
+    // Function to find and print LCS
+    static String findLCS(String X, String Y) {
+        int m = X.length();
+        int n = Y.length();
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        // Reconstructing LCS
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                lcs.append(X.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        
+        return lcs.reverse().toString();
+    }
+    
+    public static void main(String[] args) {
+        String X = "AGGTAB";
+        String Y = "GXTXAYB";
+        
+        System.out.println("LCS: " + findLCS(X, Y));
+    }
+}
