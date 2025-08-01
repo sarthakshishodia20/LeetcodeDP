c77ec152245676de838d9501bee6c5a43b04df87
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-08-01 13:45:28 +0530

    Create MAXIMALSQUARENEETCODE.java

---
+++ MAXIMALSQUARENEETCODE.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int max = 0;
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = matrix[i][0] == '1' ? 1 : 0;
+            max = Math.max(max, dp[i][0]);
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j] == '1' ? 1 : 0;
+            max = Math.max(max, dp[0][j]);
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i-1][j], 
+                                Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return max * max;
+    }
+}
